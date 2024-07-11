4f43341d0c2396bce5e53e7c679317b0df4c73a8
Aruthraa <aruthrasenthilkumar108@gmail.com>
2024-07-11 10:30:50 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,11 @@ import React, { useState } from 'react';
 import Login from './components/forms/Login';
 import Signup from './components/forms/Signup';
 import Decide from './components/forms/Decide';
-import BuyerUI from './components/Buyer UI/buyerUI';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
     return (
-<<<<<<< HEAD
-        <div>
-            <BuyerUI/>
-        </div>
-=======
         <>
             <BrowserRouter>
                 <Routes>
@@ -23,7 +17,6 @@ const App = () => {
             </BrowserRouter>
             
         </>
->>>>>>> 2ae5f3decfea812c017fb3d699032497d0db01ec
     );
 };
 
--- src/components/forms/Login.jsx
+++ src/components/forms/Login.jsx
@@ -5,14 +5,6 @@ import "./Login.css";
 import { BrowserRouter, Link, Routes } from 'react-router-dom';
 
 const Login = () => {
-<<<<<<< HEAD
-  const gotoSignup = () => {
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <Signup/>
-    )
-  }
-=======
->>>>>>> 2ae5f3decfea812c017fb3d699032497d0db01ec
   return (
     <section className='login-main'>
       <div className='login-box'>
@@ -25,13 +17,9 @@ const Login = () => {
         <p className='forgot-pw'> <a href=""> Forgot password?</a></p>
       </div>
       <div className='signup-box-alternate'>
-<<<<<<< HEAD
-        <p>Don't have an account? <a href='#signup' onClick={gotoSignup}>Sign up</a></p>
-=======
           <p>Don't have an account?
-              <Link to='/signup'>Sign up</Link>
+              <Link to='/signup'> Sign up</Link>
           </p>
->>>>>>> 2ae5f3decfea812c017fb3d699032497d0db01ec
       </div>
     </section>
   )
