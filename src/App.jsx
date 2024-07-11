23b8c41e68faddaf5920b121f09564e92f8d08e4
Aruthraa <aruthrasenthilkumar108@gmail.com>
2024-07-11 11:58:15 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import Login from './components/forms/Login';
 import Signup from './components/forms/Signup';
 import Decide from './components/forms/Decide';
+// import BuyerUI from './components/Buyer UI/buyerUI';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
+
+    console.log("dsf")
     return (
         <>
             <BrowserRouter>
@@ -15,7 +18,7 @@ const App = () => {
                     <Route path='/decide' element={ <Decide />} />
                 </Routes>
             </BrowserRouter>
-            
+            {/* <BuyerUI /> */}
         </>
     );
 };
--- src/components/Buyer UI/BuyerUI.jsx
+++ src/components/Buyer UI/BuyerUI.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import MsgMain from "./Messages/msgMain";
 
 const BuyerUI = () => {
     return (
         <>
-        <MsgMain />
         </>
     )
 }
